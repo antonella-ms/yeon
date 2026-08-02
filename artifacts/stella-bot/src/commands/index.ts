1643e4f33bda1666eb6ef35764cfa4920950525a
antonella-ms <4nton3ll4ms@gmail.com>
2026-08-02 20:01:16 -0300

    Update index.ts

--- artifacts/stella-bot/src/commands/index.ts
+++ artifacts/stella-bot/src/commands/index.ts
@@ -1,1 +1,10 @@
+import type { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import * as resetCooldown from "./reset_cooldown";
+import * as gift from "./gift";
 
+export type Command = {
+  data: SlashCommandBuilder;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+};
+
+export const commands: Command[] = [resetCooldown, gift] as unknown as Command[];
